1956dd964d885d7feb46b976a7eaf6aabffacda2
farazmd139 <faraz828732@gmail.com>
2025-08-03 17:24:02 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Service Worker Code v2
+// Service Worker Code
 const CACHE_NAME = 'quran-app-cache-v2';
 const urlsToCache = [
   '/',
@@ -9,7 +9,6 @@ const urlsToCache = [
   '/manifest.json'
 ];
 
-// Install event: open the cache and add essential files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -20,54 +19,11 @@ self.addEventListener('install', event => {
   );
 });
 
-// Activate event: remove old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
-      );
-    })
-  );
-});
-
-// Fetch event: serve cached content when offline, otherwise fetch from network
 self.addEventListener('fetch', event => {
-  // We only want to cache GET requests.
-  if (event.request.method !== 'GET') {
-    return;
-  }
-  
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response from cache
-        if (response) {
-          return response;
-        }
-
-        // Not in cache - fetch from network, then cache it
-        return fetch(event.request).then(
-          networkResponse => {
-            // Check if we received a valid response
-            if(!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
-              return networkResponse;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            const responseToCache = networkResponse.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return networkResponse;
-          }
-        );
+        return response || fetch(event.request);
       })
   );
 });
